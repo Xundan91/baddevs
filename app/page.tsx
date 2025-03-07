cc5c69e549310bcec6dcb4e02bf9c87e30ea9b9f
Xundan91 <kundansingh023230@gmail.com>
2025-03-07 22:53:30 +0530

    removed contact

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
       <ServicesSection />
       <ProductsSection />
       {/* <TeamSection /> */}
-      <ContactSection />
+      {/* <ContactSection /> */}
       <Footer />
     </main>
   );
